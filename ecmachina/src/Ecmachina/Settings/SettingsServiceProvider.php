3d0bcae863a5f934ded758a670c9d90aa6756c57
Viktor Nemeth <szeki.mail@gmail.com>
2017-07-08 11:02:58 +0200

    settings

---
+++ ecmachina/src/Ecmachina/Settings/SettingsServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ecmachina\Settings;
+
+use Illuminate\Support\ServiceProvider;
+
+class SettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
